4746c6badc179c3f0db70133938fe1f49bb84034
Eric Biewener <eric.biewener0@walmart.com>
2023-01-20 12:41:03 -0800

    timeout is reproduced

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { waitForElementToBeRemoved } from "@testing-library/react";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test("waitForElementToBeRemoved does not time out", async () => {
+  const parent = document.createElement("div");
+  const child = document.createElement("div");
+  parent.appendChild(child)
+  await waitForElementToBeRemoved(child);
 });
